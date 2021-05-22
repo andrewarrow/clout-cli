d56fa1fe88111a7f38f5baa2ee7ed95579d174c4
Andrew Arrow <oneone@gmail.com>
2021-05-22 11:59:14 -0700

    key

--- keys/hdkey.go
+++ keys/hdkey.go
@@ -10,6 +10,21 @@ import (
 
 func ComputeKeysFromSeed(seedBytes []byte) {
 	netParams := &chaincfg.MainNetParams
-	masterKey, err := hdkeychain.NewMaster(seedBytes, netParams)
-	fmt.Println("hi", masterKey, err)
+	masterKey, _ := hdkeychain.NewMaster(seedBytes, netParams)
+	index := uint32(0)
+
+	purpose, _ := masterKey.Child(hdkeychain.HardenedKeyStart + 44)
+	coinTypeKey, _ := purpose.Child(hdkeychain.HardenedKeyStart + 0)
+	accountKey, _ := coinTypeKey.Child(hdkeychain.HardenedKeyStart + 0)
+	changeKey, _ := accountKey.Child(0)
+	addressKey, _ := changeKey.Child(index)
+	pubKey, _ := addressKey.ECPubKey()
+	privKey, _ := addressKey.ECPrivKey()
+	addressObj, _ := addressKey.Address(netParams)
+	btcDepositAddress := addressObj.EncodeAddress()
+
+	fmt.Println("pubKey", pubKey)
+	fmt.Println("privKey", privKey)
+	fmt.Println("addressObj", addressObj)
+	fmt.Println("btcDepositAddress", btcDepositAddress)
 }
