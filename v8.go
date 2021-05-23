ce2183e34c60757ebb2efb0072031c96a52468c1
Andrew Arrow <oneone@gmail.com>
2021-05-23 10:19:20 -0700

    v8

--- v8.go
+++ v8.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 
 	"rogchap.com/v8go"
 )
 
 func RunV8() {
 	ctx, _ := v8go.NewContext()
-	ctx.RunScript("const add = (a, b) => a + b", "math.js")
-	ctx.RunScript("const result = add(13, 4)", "main.js")
+
+	b, _ := ioutil.ReadFile("identity/runtime.js")
+	runtime := string(b)
+	b, _ = ioutil.ReadFile("identity/polyfills.js")
+	polyfills := string(b)
+	b, _ = ioutil.ReadFile("identity/vendor.js")
+	vendor := string(b)
+	b, _ = ioutil.ReadFile("identity/main.js")
+	main := string(b)
+
+	ctx.RunScript(runtime, "runtime.js")
+	ctx.RunScript(polyfills, "polyfills.js")
+	ctx.RunScript(vendor, "vendor.js")
+	ctx.RunScript(main, "main.js")
+
+	ctx.RunScript("const result = 18", "more.js")
+
 	val, _ := ctx.RunScript("result", "value.js")
 	fmt.Printf("addition result: %s", val)
 }
