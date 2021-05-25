a8a26197813656875ff07b0b408c76675bd8c7f0
Andrew Arrow <oneone@gmail.com>
2021-05-25 08:37:42 -0700

    output usernames in notification list

--- notification.go
+++ notification.go
@@ -7,12 +7,14 @@ import (
 	"fmt"
 )
 
-func ParseUserList(js string) {
+func ParseUserList(js string) map[string]string {
+	m := map[string]string{}
 	var us models.UsersStateless
 	json.Unmarshal([]byte(js), &us)
 	for _, u := range us.UserList {
-		fmt.Println(u.ProfileEntryResponse.Username)
+		m[u.ProfileEntryResponse.PublicKeyBase58Check] = u.ProfileEntryResponse.Username
 	}
+	return m
 }
 func ListNotifications() {
 	pub58 := LoggedInPub58()
@@ -21,6 +23,7 @@ func ListNotifications() {
 	var list models.NotificationList
 	json.Unmarshal([]byte(js), &list)
 	mapOfUsers := map[string]bool{}
+	pub58ToUsernames := map[string]string{}
 	buff := []string{}
 	for _, n := range list.Notifications {
 		mapOfUsers[n.Metadata.TransactorPublicKeyBase58Check] = true
@@ -29,18 +32,27 @@ func ListNotifications() {
 	for k, _ := range mapOfUsers {
 		buff = append(buff, k)
 		if len(buff) == 10 {
+			fmt.Println("fetching 10...")
 			js = GetManyUsersStateless(buff)
-			ParseUserList(js)
+			m := ParseUserList(js)
+			for k, v := range m {
+				pub58ToUsernames[k] = v
+			}
 			buff = []string{}
 		}
 	}
 	if len(buff) > 0 {
+		fmt.Println("fetching ", len(buff))
 		js = GetManyUsersStateless(buff)
-		ParseUserList(js)
+		m := ParseUserList(js)
+		for k, v := range m {
+			pub58ToUsernames[k] = v
+		}
 	}
 
 	for i, n := range list.Notifications {
-		fmt.Printf("%02d %s %s\n", i, display.LeftAligned(n.Metadata.TxnType, 30),
+		fmt.Printf("%02d %s %s %s\n", i, display.LeftAligned(n.Metadata.TxnType, 30),
+			pub58ToUsernames[n.Metadata.TransactorPublicKeyBase58Check],
 			n.Metadata.CreatorCoinTransferTxindexMetadata.CreatorUsername)
 	}
 }
