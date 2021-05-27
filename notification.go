df70479014a19ce7a7b417b8196b25f071b1f19f
Andrew Arrow <oneone@gmail.com>
2021-05-26 21:16:03 -0700

    display the parent post for SUBMIT_POST

--- notification.go
+++ notification.go
@@ -84,12 +84,15 @@ func ListNotificationForPub(pub58 string) {
 			n.Metadata.CreatorCoinTransferTxindexMetadata.CreatorUsername)
 
 		if n.Metadata.TxnType == "SUBMIT_POST" {
+			parent := list.PostsByHash[n.Metadata.SubmitPostTxindexMetadata.ParentPostHashHex]
 			p := list.PostsByHash[n.Metadata.SubmitPostTxindexMetadata.PostHashBeingModifiedHex]
 			ts := time.Unix(p.TimestampNanos/1000000000, 0)
 			ago := timeago.FromDuration(time.Since(ts))
 			//fmt.Println(n.Metadata.SubmitPostTxindexMetadata.PostHashBeingModifiedHex)
-			tokens := strings.Split(p.Body, "\n")
-			fmt.Println(display.LeftAligned(tokens[0], 60), "     ", ago)
+			tokens := strings.Split(parent.Body, "\n")
+			fmt.Println(display.LeftAligned(tokens[0], 60))
+			tokens = strings.Split(p.Body, "\n")
+			fmt.Println(display.LeftAligned("  "+tokens[0], 60), "     ", ago)
 		} else if n.Metadata.TxnType == "LIKE" {
 			p := list.PostsByHash[n.Metadata.LikeTxindexMetadata.PostHashHex]
 			ts := time.Unix(p.TimestampNanos/1000000000, 0)
