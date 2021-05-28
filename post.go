f53e60a7c6ec42abcd52292557f30845383b0d41
Andrew Arrow <oneone@gmail.com>
2021-05-28 09:46:52 -0700

    listing a single post with it's replies

--- post.go
+++ post.go
@@ -31,20 +31,36 @@ func ShowSinglePost(key string) {
 	var ps models.PostStateless
 	json.Unmarshal([]byte(js), &ps)
 
+	fmt.Println("")
 	fmt.Println(ps.PostFound.Body)
+	fmt.Println("")
+
+	fmt.Printf("%s %s %s\n", display.LeftAligned("username", 20),
+		display.LeftAligned("ago", 15),
+		display.LeftAligned("hash", 10))
+	fmt.Printf("%s %s %s\n", display.LeftAligned("--------", 20),
+		display.LeftAligned("---", 15),
+		display.LeftAligned("--------", 10))
 	for i, p := range ps.PostFound.Comments {
 		ts := time.Unix(p.TimestampNanos/1000000000, 0)
 		ago := timeago.FromDuration(time.Since(ts))
-		fmt.Println(display.LeftAligned(p.ProfileEntryResponse.Username, 30),
-			display.LeftAligned(p.ProfileEntryResponse.CoinEntry.NumberOfHolders, 20),
-			ago)
-		tokens := strings.Split(p.Body, "\n")
-		fmt.Println("        ", display.LeftAligned(tokens[0], 40))
-		fmt.Println("")
-		if i > 6 {
+		//fmt.Println(display.LeftAligned(p.ProfileEntryResponse.Username, 30),
+		//	display.LeftAligned(p.ProfileEntryResponse.CoinEntry.NumberOfHolders, 20),
+		//	ago)
+		//tokens := strings.Split(p.Body, "\n")
+		//fmt.Println("        ", display.LeftAligned(tokens[0], 40))
+		//fmt.Println("")
+		username := p.ProfileEntryResponse.Username
+		short := p.PostHashHex[0:7]
+		fmt.Printf("%s %s %s\n", display.LeftAligned(username, 20),
+			display.LeftAligned(ago, 15),
+			display.LeftAligned(short, 10))
+
+		if i > 20 {
 			break
 		}
 	}
+	fmt.Println("")
 }
 
 func PostsForPublicKey(key string) {
