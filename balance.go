64330c3ebcc99c584fe9e76d0dfee1bb2bc50cd0
Andrew Arrow <oneone@gmail.com>
2021-05-30 08:37:15 -0700

    adding CreatorBasisPoints to balance output

--- balance.go
+++ balance.go
@@ -14,8 +14,10 @@ func HandleBalances(argMap map[string]string) {
 		fmt.Println(username)
 		pub58, _ := keys.ComputeKeysFromSeed(session.SeedBytes(s))
 		user := session.Pub58ToUser(pub58)
+		points := user.ProfileEntryResponse.CoinEntry.CreatorBasisPoints
 		fmt.Printf("  %s %.02f\n", display.LeftAligned("BalanceNano", 20), float64(user.BalanceNanos)/1000000.0)
 		fmt.Printf("  %s %.02f\n", display.LeftAligned("MarketCap", 20), user.ProfileEntryResponse.MarketCap())
+		fmt.Printf("  %s %d\n", display.LeftAligned("Points", 20), points)
 	}
 	fmt.Println("")
 }
