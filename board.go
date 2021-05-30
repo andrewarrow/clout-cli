b421335ddd30bb4471aff0d9687000246096757a
Andrew Arrow <oneone@gmail.com>
2021-05-30 08:13:16 -0700

    moving code from sessions to board + total at top

--- board.go
+++ board.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"clout/display"
 	"clout/keys"
+	"clout/models"
+	"clout/network"
 	"clout/session"
+	"encoding/json"
 	"fmt"
 )
 
@@ -20,7 +24,41 @@ func HandleBoards() {
 		fmt.Println(username)
 		fmt.Println("===========")
 		pub58, _ := keys.ComputeKeysFromSeed(session.SeedBytes(s))
-		session.Pub58ToBoards(pub58)
+		Pub58ToBoards(pub58)
 	}
 	fmt.Println("")
+}
+
+func Pub58ToBoards(key string) {
+	js := network.GetUsersStateless(key)
+	var us models.UsersStateless
+	json.Unmarshal([]byte(js), &us)
+	for _, thing := range us.UserList[0].UsersYouHODL {
+		coins := float64(thing.BalanceNanos) / 1000000000.0
+		if coins < 1 {
+			continue
+		}
+		fmt.Printf("%s %0.2f\n",
+			display.LeftAligned(thing.ProfileEntryResponse.Username, 30), coins)
+
+		other := thing.ProfileEntryResponse.PublicKeyBase58Check
+		js = network.GetUsersStateless(other)
+		var us models.UsersStateless
+		json.Unmarshal([]byte(js), &us)
+		for _, friend := range us.UserList[0].UsersWhoHODLYou {
+			if friend.ProfileEntryResponse.PublicKeyBase58Check == key {
+				continue
+			}
+			coins := float64(friend.BalanceNanos) / 1000000000.0
+			if coins < 1 {
+				continue
+			}
+			username := friend.ProfileEntryResponse.Username
+			if username == "" {
+				username = "anonymous"
+			}
+			fmt.Printf("  %s %0.2f\n",
+				display.LeftAligned(username, 30), coins)
+		}
+	}
 }
--- wallet.go
+++ wallet.go
@@ -22,20 +22,33 @@ func ListAssets(key string) {
 	json.Unmarshal([]byte(js), &us)
 	fields := []string{"username", "balance", "price", "worth"}
 	sizes := []int{20, 11, 10, 10}
-	display.Header(sizes, fields...)
 
 	YouHODL := us.UserList[0].UsersYouHODL
 	sort.SliceStable(YouHODL, func(i, j int) bool {
 		return YouHODL[i].BalanceNanos > YouHODL[j].BalanceNanos
 	})
 
+	total := 0.0
 	for _, thing := range YouHODL {
+		value := display.OneE9Float(thing.ProfileEntryResponse.CoinPriceBitCloutNanos) *
+			display.OneE9Float(thing.BalanceNanos)
+		total += value
+	}
+	display.Row(sizes,
+		"",
+		"",
+		"",
+		display.Float(total))
+
+	display.Header(sizes, fields...)
+	for _, thing := range YouHODL {
+		value := display.OneE9Float(thing.ProfileEntryResponse.CoinPriceBitCloutNanos) *
+			display.OneE9Float(thing.BalanceNanos)
 		display.Row(sizes,
 			thing.ProfileEntryResponse.Username,
 			display.OneE9(thing.BalanceNanos),
 			display.OneE9(thing.ProfileEntryResponse.CoinPriceBitCloutNanos),
-
-			display.Float(display.OneE9Float(thing.ProfileEntryResponse.CoinPriceBitCloutNanos)*
-				display.OneE9Float(thing.BalanceNanos)))
+			display.Float(value))
 	}
+
 }
