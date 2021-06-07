f5cfe63bc7468d64cddb8086d7053b19deb722ef
Andrew Arrow <oneone@gmail.com>
2021-06-07 10:05:31 -0700

    balance output fixes showing % owned

--- balance.go
+++ balance.go
@@ -9,41 +9,40 @@ import (
 )
 
 func HandleBalances(argMap map[string]string) {
+	list := session.ReadAccountsSorted()
 	m := session.ReadAccounts()
-	for username, s := range m {
-		fmt.Println("")
+	fields := []string{"username", "% owned", "co-owner1", "co-owner2", "balance", "points"}
+	sizes := []int{15, 10, 15, 15, 10, 10}
+	display.Header(sizes, fields...)
+	for _, username := range list {
+		s := m[username]
 		pub58, _ := keys.ComputeKeysFromSeed(session.SeedBytes(s))
-		fmt.Println(username, pub58)
 		user := session.Pub58ToUser(pub58)
 		points := user.ProfileEntryResponse.CoinEntry.CreatorBasisPoints
 		total := user.ProfileEntryResponse.CoinEntry.CoinsInCirculationNanos
 
-		fmt.Printf("  %s %.02f\n", display.LeftAligned("BalanceNano", 20), float64(user.BalanceNanos)/1000000.0)
-		fmt.Printf("  %s %.02f\n", display.LeftAligned("MarketCap", 20), user.ProfileEntryResponse.MarketCap())
-		fmt.Printf("  %s %d\n", display.LeftAligned("Points", 20), points)
-		fmt.Printf("  %s %s\n", display.LeftAligned("Price", 20), display.OneE9(user.ProfileEntryResponse.CoinPriceBitCloutNanos))
-
+		holdMap := map[string]string{}
 		sort.SliceStable(user.UsersWhoHODLYou, func(i, j int) bool {
 			return user.UsersWhoHODLYou[i].BalanceNanos >
 				user.UsersWhoHODLYou[j].BalanceNanos
 		})
-		for i, friend := range user.UsersWhoHODLYou {
-			coins := float64(friend.BalanceNanos) / 1000000000.0
+		topThree := []string{}
+		for _, friend := range user.UsersWhoHODLYou {
 			username := friend.ProfileEntryResponse.Username
 			if username == "" {
-				username = "anonymous"
-			}
-			fmt.Printf("  %s %0.6f %0.6f\n",
-				display.LeftAligned(username, 30), coins,
-				float64(friend.BalanceNanos)/float64(total))
-			if username == "anonymous" {
-				fmt.Println(friend.HODLerPublicKeyBase58Check)
+				username = friend.HODLerPublicKeyBase58Check
 			}
 
-			if i > 9 {
-				break
+			perString := fmt.Sprintf("%3d",
+				int(100*(float64(friend.BalanceNanos)/float64(total))))
+			holdMap[username] = perString
+
+			if len(topThree) < 3 {
+				topThree = append(topThree, username)
 			}
 		}
+
+		display.Row(sizes, username, holdMap[username], "", "",
+			display.OneE9(user.BalanceNanos), float64(points)/100.0)
 	}
-	fmt.Println("")
 }
