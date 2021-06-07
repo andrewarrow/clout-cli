90d35a1d51f9e8cca9af0b34888440d333655978
Andrew Arrow <oneone@gmail.com>
2021-06-06 21:12:43 -0700

    show % ownership using total

--- balance.go
+++ balance.go
@@ -24,18 +24,22 @@ func HandleBalances(argMap map[string]string) {
 		fmt.Println(username, pub58)
 		user := session.Pub58ToUser(pub58)
 		points := user.ProfileEntryResponse.CoinEntry.CreatorBasisPoints
+		total := user.ProfileEntryResponse.CoinEntry.CoinsInCirculationNanos
+
 		fmt.Printf("  %s %.02f\n", display.LeftAligned("BalanceNano", 20), float64(user.BalanceNanos)/1000000.0)
 		fmt.Printf("  %s %.02f\n", display.LeftAligned("MarketCap", 20), user.ProfileEntryResponse.MarketCap())
 		fmt.Printf("  %s %d\n", display.LeftAligned("Points", 20), points)
 		fmt.Printf("  %s %s\n", display.LeftAligned("Price", 20), display.OneE9(user.ProfileEntryResponse.CoinPriceBitCloutNanos))
+
 		for _, friend := range user.UsersWhoHODLYou {
 			coins := float64(friend.BalanceNanos) / 1000000000.0
 			username := friend.ProfileEntryResponse.Username
 			if username == "" {
 				username = "anonymous"
 			}
-			fmt.Printf("  %s %0.6f\n",
-				display.LeftAligned(username, 30), coins)
+			fmt.Printf("  %s %0.6f %0.6f\n",
+				display.LeftAligned(username, 30), coins,
+				float64(friend.BalanceNanos)/float64(total))
 			if username == "anonymous" {
 				fmt.Println(friend.HODLerPublicKeyBase58Check)
 			}
