8ef54a01ad74cb309c1c97e5e275a3d0b69aa5a7
Andrew Arrow <oneone@gmail.com>
2021-06-07 10:23:21 -0700

    showing ownership % for all accounts

--- balance.go
+++ balance.go
@@ -11,14 +11,14 @@ import (
 func HandleBalances(argMap map[string]string) {
 	list := session.ReadAccountsSorted()
 	m := session.ReadAccounts()
-	fields := []string{"username", "% owned", "co-owner1", "co-owner2", "balance", "points"}
-	sizes := []int{15, 10, 15, 15, 10, 10}
+	fields := []string{"username", "% owned", "co-owner1", "co-owner2", "balance"}
+	sizes := []int{15, 10, 20, 20, 10}
 	display.Header(sizes, fields...)
 	for _, username := range list {
 		s := m[username]
 		pub58, _ := keys.ComputeKeysFromSeed(session.SeedBytes(s))
 		user := session.Pub58ToUser(pub58)
-		points := user.ProfileEntryResponse.CoinEntry.CreatorBasisPoints
+		//points := user.ProfileEntryResponse.CoinEntry.CreatorBasisPoints
 		total := user.ProfileEntryResponse.CoinEntry.CoinsInCirculationNanos
 
 		holdMap := map[string]string{}
@@ -30,10 +30,10 @@ func HandleBalances(argMap map[string]string) {
 		for _, friend := range user.UsersWhoHODLYou {
 			username := friend.ProfileEntryResponse.Username
 			if username == "" {
-				username = friend.HODLerPublicKeyBase58Check
+				username = "anonymous" //friend.HODLerPublicKeyBase58Check
 			}
 
-			perString := fmt.Sprintf("%3d",
+			perString := fmt.Sprintf("%d",
 				int(100*(float64(friend.BalanceNanos)/float64(total))))
 			holdMap[username] = perString
 
@@ -42,7 +42,25 @@ func HandleBalances(argMap map[string]string) {
 			}
 		}
 
-		display.Row(sizes, username, holdMap[username], "", "",
-			display.OneE9(user.BalanceNanos), float64(points)/100.0)
+		others := []string{}
+		for _, u := range topThree {
+			if u == username {
+				continue
+			}
+			others = append(others, u)
+		}
+
+		co1 := ""
+		co2 := ""
+
+		if len(others) == 2 {
+			co1 = others[0] + " " + holdMap[others[0]]
+			co2 = others[1] + " " + holdMap[others[1]]
+		} else if len(others) == 1 {
+			co1 = others[0] + " " + holdMap[others[0]]
+		}
+
+		display.Row(sizes, username, holdMap[username], co1, co2,
+			display.OneE9(user.BalanceNanos))
 	}
 }
