0a58330e29f76add297ac6e438772ebd80ce4c21
Andrew Arrow <oneone@gmail.com>
2021-06-06 17:34:01 -0700

    adding cut section to youtube

--- youtube.go
+++ youtube.go
@@ -8,28 +8,62 @@ import (
 	"time"
 )
 
-func HandleYoutube() {
-	id := argMap["id"]
-	fmt.Println(id)
+func SetupYoutubeDirectory() string {
 	home := files.UserHomeDir()
 	dir := "clout-cli-youtube"
 	path := home + "/" + dir
 	os.Mkdir(path, 0700)
-	cmd := exec.Command("youtube-dl", "--output",
-		path+"/%(id)s.%(ext)s",
-		"--recode-video", "mp4", id)
+	return path
+}
+func HandleYoutube() {
+	id := argMap["id"]
+	fmt.Println(id)
 
-	//var out bytes.Buffer
-	//cmd.Stdout = &out
+	action := argMap["action"]
+
+	if action == "" {
+		action = "download"
+	}
+
+	if action == "download" {
+		DownloadYoutube(id)
+	} else if action == "cut" {
+		CutUpFile(id)
+	}
+
+}
+
+func CutUpFile(id string) {
+	path := SetupYoutubeDirectory()
+	cmd := exec.Command("ffmpeg", "-i", path+"/"+id+".mp4", "-ss", "0", "-t", "60",
+		path+"/"+id+".60.mp4")
 
 	go cmd.Run()
 
 	for {
-		b, _ := exec.Command("ls", "-lh", path).CombinedOutput()
-		s := string(b)
-		fmt.Println(s)
-		fmt.Println("")
+		PrintDirectoryInfo(path)
 		time.Sleep(time.Second * 5)
 	}
+}
 
+func DownloadYoutube(id string) {
+	path := SetupYoutubeDirectory()
+
+	cmd := exec.Command("youtube-dl", "--output",
+		path+"/%(id)s.%(ext)s",
+		"--recode-video", "mp4", id)
+
+	go cmd.Run()
+
+	for {
+		PrintDirectoryInfo(path)
+		time.Sleep(time.Second * 5)
+	}
+}
+
+func PrintDirectoryInfo(path string) {
+	b, _ := exec.Command("ls", "-lh", path).CombinedOutput()
+	s := string(b)
+	fmt.Println(s)
+	fmt.Println("")
 }
