215c37dd9868799dc3610674b24e94653e1d137c
Andrew Arrow <oneone@gmail.com>
2021-06-08 03:46:11 -0700

    calling w.Terminate vs w.Destroy

--- inspect.go
+++ inspect.go
@@ -29,30 +29,36 @@ func HandleInspect() {
 			fmt.Println(token)
 
 			if strings.Contains(token, "twitter.com/") {
-				debug := true
-				w := webview.New(debug)
-				defer w.Destroy()
-				w.SetTitle("cloutcli")
-				w.SetSize(800, 600, webview.HintNone)
-				w.Navigate(token)
-				w.Bind("sendBackBodyInnerHTML", callback)
-
-				w.Dispatch(func() {
-					go func() {
-						time.Sleep(time.Second * 4)
-						w.Eval("sendBackBodyInnerHTML(document.body.innerHTML);")
-					}()
-				})
-				w.Run()
+				HandleTwitterGrab(token)
 			}
 		}
 	}
 }
 
-func callback(data string) {
+var w webview.WebView
+
+func HandleTwitterGrab(url string) {
+	debug := true
+	w = webview.New(debug)
+	w.SetTitle("cloutcli")
+	w.SetSize(800, 600, webview.HintNone)
+	w.Navigate(url)
+	w.Bind("sendBackBodyInnerHTML", twitterCallback)
+
+	w.Dispatch(func() {
+		go func() {
+			time.Sleep(time.Second * 4)
+			w.Eval("sendBackBodyInnerHTML(document.body.innerHTML);")
+		}()
+	})
+	w.Run()
+}
+
+func twitterCallback(data string) {
 	tokens := strings.Split(data, "followers")
 	tokens = strings.Split(tokens[1], ">")
 	tokens = strings.Split(tokens[3], "<")
 	fmt.Println("followers:", tokens[0])
-	os.Exit(0)
+	w.Terminate()
+	//w.Destroy()
 }
