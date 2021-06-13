443742d8453eff8bb0051141da8a765c042628c5
Andrew Arrow <oneone@gmail.com>
2021-06-13 13:09:43 -0700

    drawing lines

--- draw/diamond.go
+++ draw/diamond.go
@@ -9,7 +9,18 @@ func DrawDiamond() {
 	dc.SetRGB(0, 0, 0)
 
 	dc.SetLineWidth(2)
-	dc.DrawRectangle(50, 50, 100, 100)
+	startX := float64(25)
+	startY := float64(100)
+	//sizeX := float64(100)
+	//sizeY := float64(50)
+	size := float64(50)
+	dc.DrawLine(startX, startY, startX+size, startY+size)
+	dc.DrawLine(startX+size, startY+size, startX+size+size, startY)
+	dc.DrawLine(startX+size+size, startY, startX+size, startY-size)
+	dc.DrawLine(startX+size, startY-size, startX, startY)
+
+	dc.DrawLine(startX, startY, startX+size, startY+size+size+size)
+	dc.DrawLine(startX+size, startY+size+size+size, startX+size+size, startY)
 	dc.Stroke()
 	dc.SavePNG("001.png")
 }
